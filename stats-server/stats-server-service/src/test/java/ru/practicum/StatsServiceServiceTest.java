97e614001e1426d24d87f532f7479eef77c1715d
Светлана <kulechko@mail.ru>
2023-11-21 22:01:30 +0300

    ТЗ

--- stats-server/stats-server-service/src/test/java/ru/practicum/StatsServiceServiceTest.java
+++ stats-server/stats-server-service/src/test/java/ru/practicum/StatsServiceServiceTest.java
@@ -15,6 +15,7 @@ import javax.persistence.NoResultException;
 import javax.persistence.TypedQuery;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -31,6 +32,7 @@ public class StatsServiceServiceTest {
     private final EntityManager em;
     private final StatsService statsService;
     private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private final ApplicationRepository applicationRepository;
 
 
     @Test
@@ -91,5 +93,112 @@ public class StatsServiceServiceTest {
         } catch (NoResultException e) {
            return;
         }
+    }
+
+    @Test
+    void shouldGetStats() {
+        // Sample endpoint hits
+        EndpointHit endpointHit1 = EndpointHit.builder()
+                .app("ewm-main-service")
+                .uri("/events/1")
+                .ip("192.163.0.1")
+                .timestamp("2022-09-06 11:00:23")
+                .build();
+
+        EndpointHit endpointHit2 = EndpointHit.builder()
+                .app("ewm-main-service")
+                .uri("/events")
+                .ip("192.163.0.1")
+                .timestamp("2023-02-06 11:00:23")
+                .build();
+
+        EndpointHit endpointHit3 = EndpointHit.builder()
+                .app("ewm-main-service")
+                .uri("/events/1")
+                .ip("192.163.0.1")
+                .timestamp("2023-03-06 11:00:23")
+                .build();
+
+        EndpointHit endpointHit4 = EndpointHit.builder()
+                .app("ewm-main-service")
+                .uri("/events/1")
+                .ip("192.163.0.2")
+                .timestamp("2023-05-06 11:00:23")
+                .build();
+
+        // Create endpoint hits
+        statsService.create(endpointHit1);
+        statsService.create(endpointHit2);
+        statsService.create(endpointHit3);
+        statsService.create(endpointHit4);
+
+        // Вывод статистики при отсутствии критериев к uri
+        List<ViewStats> resultWithoutUris = statsService.get(
+                LocalDateTime.parse("2021-05-06 11:00:23", DATE_FORMATTER),
+                LocalDateTime.parse("2023-07-06 11:00:23", DATE_FORMATTER),
+                new String[]{"all"},
+                false);
+
+        assertThat(resultWithoutUris, is(notNullValue()));
+        assertThat(resultWithoutUris.size(), is(2));
+        assertThat(resultWithoutUris.get(0).getAppName(), is(equalTo("ewm-main-service")));
+        assertThat(resultWithoutUris.get(0).getUri(), is(equalTo("/events/1")));
+        assertThat(resultWithoutUris.get(0).getHits(), is(3));
+        assertThat(resultWithoutUris.get(1).getAppName(), is(equalTo("ewm-main-service")));
+        assertThat(resultWithoutUris.get(1).getUri(), is(equalTo("/events")));
+        assertThat(resultWithoutUris.get(1).getHits(), is(1));
+
+        // Вывод статистики с списком uri
+        List<ViewStats> resultWithAllUris = statsService.get(
+                LocalDateTime.parse("2021-05-06 11:00:23", DATE_FORMATTER),
+                LocalDateTime.parse("2023-07-06 11:00:23", DATE_FORMATTER),
+                new String[]{"/events", "/events/1"},
+                false);
+
+        assertThat(resultWithAllUris, is(notNullValue()));
+        assertThat(resultWithAllUris.size(), is(2));
+        assertThat(resultWithAllUris, is(equalTo(resultWithoutUris)));
+
+        // Вывод статистики с 1 значением uri
+        List<ViewStats> resultWithOneUris = statsService.get(
+                LocalDateTime.parse("2021-05-06 11:00:23", DATE_FORMATTER),
+                LocalDateTime.parse("2023-07-06 11:00:23", DATE_FORMATTER),
+                new String[]{"/events/1"},
+                false);
+
+        assertThat(resultWithOneUris, is(notNullValue()));
+        assertThat(resultWithOneUris.size(), is(1));
+        assertThat(resultWithOneUris.get(0).getAppName(), is(equalTo("ewm-main-service")));
+        assertThat(resultWithOneUris.get(0).getUri(), is(equalTo("/events/1")));
+        assertThat(resultWithOneUris.get(0).getHits(), is(3));
+
+        // Вывод статистики с уникальными значениями ip
+        List<ViewStats> resultWithUniqueIp = statsService.get(
+                LocalDateTime.parse("2021-05-06 11:00:23", DATE_FORMATTER),
+                LocalDateTime.parse("2023-07-06 11:00:23", DATE_FORMATTER),
+                new String[]{"/events/1"},
+                true);
+
+        assertThat(resultWithUniqueIp, is(notNullValue()));
+        assertThat(resultWithUniqueIp.size(), is(1));
+        assertThat(resultWithUniqueIp.get(0).getAppName(), is(equalTo("ewm-main-service")));
+        assertThat(resultWithUniqueIp.get(0).getUri(), is(equalTo("/events/1")));
+        assertThat(resultWithUniqueIp.get(0).getHits(), is(2));
+
+        // Вывод статистики при ограниченном диапазоне timestamp
+        List<ViewStats> resultWithRange = statsService.get(
+                LocalDateTime.parse("2023-01-06 11:00:23", DATE_FORMATTER),
+                LocalDateTime.parse("2023-04-06 11:00:23", DATE_FORMATTER),
+                new String[]{"/events", "/events/1"},
+                false);
+
+        assertThat(resultWithRange, is(notNullValue()));
+        assertThat(resultWithRange.size(), is(2));
+        assertThat(resultWithRange.get(0).getAppName(), is(equalTo("ewm-main-service")));
+        assertThat(resultWithRange.get(0).getUri(), is(equalTo("/events")));
+        assertThat(resultWithRange.get(0).getHits(), is(1));
+        assertThat(resultWithRange.get(1).getAppName(), is(equalTo("ewm-main-service")));
+        assertThat(resultWithRange.get(1).getUri(), is(equalTo("/events/1")));
+        assertThat(resultWithRange.get(1).getHits(), is(1));
     }
 }
